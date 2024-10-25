61792a56b08263b9fe48d7d9998af7ff1332f169
Furqan Nadir <74790878+Furqan-Nadir@users.noreply.github.com>
2024-10-25 12:12:37 +0500

    Create Button_12.js

---
+++ Buttons/Button#12/Button_12/Button_12.js
@@ -0,0 +1,21 @@
+const rippleButton8 = document.getElementById('rippleButton8');
+
+rippleButton8.addEventListener('click', function (e) {
+  const ripple = document.createElement('span');
+  ripple.classList.add('ripple');
+  this.appendChild(ripple);
+
+  const rect = this.getBoundingClientRect();
+  const size = Math.max(rect.width, rect.height);
+  ripple.style.width = ripple.style.height = `${size}px`;
+
+  const x = e.clientX - rect.left - size / 2;
+  const y = e.clientY - rect.top - size / 2;
+
+  ripple.style.left = `${x}px`;
+  ripple.style.top = `${y}px`;
+
+  setTimeout(() => {
+    ripple.remove();
+  }, 1200);
+});
